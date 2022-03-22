bbe205097c5e33c355c28f88e020df80e5908c5b
Habibur Rahman <hrsshainik@gmail.com>
2022-03-23 02:03:30 +0600

    navbar half complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import images from "../../constants/images";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div>
+            <HiX onClick={() => setToggle(false)} />
+            {["home", "about", "contact", "work", "skills"].map((item) => (
+              <li className="app__flex p-text" key={`link-${item}`}>
+                <div />
+                <a href={`#${item}`}>{item}</a>
+              </li>
+            ))}
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
